b784018887a20fd63b0a19fc5a637de9b7e2172a
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-10-06 22:16:24 +0200

    Added new functions for PopupQueueManager and also now properly define globaly site constants

--- src/types/GameTypes.d.ts
+++ src/types/GameTypes.d.ts
@@ -3,7 +3,7 @@ export interface global_pop_hero_girls_incomplete {
   carac1: number;
   carac2: number;
   carac3: number;
-  id_places_of_power: number|null;
+  id_places_of_power: number | null;
 }
 
 export interface PoPGirlData {
@@ -15,18 +15,16 @@ export interface PoPGirlData {
   carac_1_initial: number;
   carac_2_initial: number;
   carac_3_initial: number;
-  id_girl : number;
+  id_girl: number;
   name: string;
   rarity: string;
 }
-
 
 export interface PlacesOfPowerReward {
   loot: boolean;
   name?: string;
   rewards: Array<any>; // Maybe to do unsure if I actually will do it
 }
-
 
 export interface PlacesOfPowerData {
   base_power: number;
@@ -61,4 +59,16 @@ export interface PlacesOfPowerData {
   time_to_finish: number; // 0 when PoP is claimable or can be started
   title: string;
   type: "standard" | "temp";
+}
+export interface popupForQueue {
+  $dom_element: JQuery<HTMLElement>; // unsure if really needed but prolly yes
+  init: (t: boolean) => void; // called first, no idea what the boolean is for
+  onOpen: () => void; // called after init
+  addEventListeners: () => void; // called right after open
+  popup_name: string;
+  close_on_esc: boolean; // wether it should close when pressing escape or not
+  type: "toast" | "common" | "sliding" | "notification";
+  onClose: () => void; // called when closing
+  removeEventListeners: () => void; // called right after closing
+  destroy: () => void; // useful when you want to put a red cross to close the popup, only called manually
 }
--- src/userscripts.d.ts
+++ src/userscripts.d.ts
@@ -1,15 +1,30 @@
-declare let unsafeWindow: UnsafeWindow;
+import { popupForQueue } from "./types/GameTypes";
 
-declare namespace GM {
-    function addStyle(css: string): HTMLStyleElement;
-    function openInTab(url: string, options?: { active: boolean }): void;
+declare global {
+    let unsafeWindow: UnsafeWindow;
+
+    namespace GM {
+        function addStyle(css: string): HTMLStyleElement;
+        function openInTab(url: string, options?: { active: boolean }): void;
+    }
+
+    const shared: {
+        PopupQueueManager: {
+            add(options: { popup: popupForQueue }): void;
+            close(options: { type: string }): void;
+        }
+        general: any;
+        reward_popup: any;
+        reward: any;
+        animations: any;
+        timer: any;
+    };
+    
+    const GT: any;
+    const IMAGES_URL: string;
+
+    function GM_addStyle(css: string): HTMLStyleElement;
+    function number_format_lang(num: number): string;
 }
 
-declare let shared: any;
-declare const GT: any;
-declare const IMAGES_URL: string;
-
-
-declare function GM_addStyle(css: string): HTMLStyleElement;
-
-declare function number_format_lang(num: number): string;
+export {};
