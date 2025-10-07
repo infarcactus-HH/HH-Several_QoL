90f15344f8f94de7739763acb22cf2562a32df97
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-10-07 16:21:32 +0200

    Fix rewards not disappearing after claiming all daily chests

--- src/modules/NoReloadFromClaimingDailyChests.ts
+++ src/modules/NoReloadFromClaimingDailyChests.ts
@@ -70,7 +70,7 @@ export default class NoReloadFromClaimingDailyChests extends HHModule {
 
   checkIfAllChestsClaimed() {
     if (
-      daily_goals_member_progression.taken_rewards_array.length >= Math.min(daily_goals_member_progression.potions_amount / 20, 5)
+      daily_goals_member_progression.taken_rewards_array.length >= Math.min( Math.floor(daily_goals_member_progression.potions_amount / 20), 5)
     ) {
       $(`[data-tab="daily_goals"] > .collect_notif`).remove();
     }
