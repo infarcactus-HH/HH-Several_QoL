c72577efef469799c20f8b57cd3c45616b1c17a1
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-10-17 23:19:57 +0200

    Restock **the** Market v2

--- src/modules/EventInfo.ts
+++ src/modules/EventInfo.ts
@@ -153,7 +153,7 @@ export default class EventInfo extends HHModule {
                     `There's one that's <strong>really annoying</strong> that can only appear on the hard side:<br>` +
                     `- Restock the Market<br>` +
                     `<br>` +
-                    `One info not given is that a challenge cannot be the same on both easy & hard side as such try to lock an annoying mission on the hard side before you get stuck with "Restock Market"` +
+                    `One info not given is that a challenge cannot be the same on both easy & hard side as such try to lock an annoying mission on the hard side before you get stuck with "Restock the Market"` +
                     `</span></div>`
                 );
             });
