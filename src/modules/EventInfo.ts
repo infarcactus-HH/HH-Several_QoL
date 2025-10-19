c71db8b71faf7f335af49c581a90e33a606b38e1
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-10-19 13:27:21 +0200

    EventInfo : Added Seasonal (ME), Season, Love raids

--- src/modules/EventInfo.ts
+++ src/modules/EventInfo.ts
@@ -1,13 +1,15 @@
 import { HHModule } from "../types/HH++";
+import { HHPlusPlusReplacer } from "../utils/HHPlusPlusreplacer";
 import EventInfo_Event from "./EventInfo/EventInfo_Event";
 import EventInfo_Home from "./EventInfo/EventInfo_Home";
 import EventInfo_Pathes from "./EventInfo/EventInfo_Pathes";
+import EventInfo_Seasonal from "./EventInfo/EventInfo_Seasonal";
 
 export default class EventInfo extends HHModule {
   readonly configSchema = {
     baseKey: "eventInfo",
     label:
-      "<span tooltip='Click on the Information top right of event (only DP, SM, CbC, OD for now)'>Event Info (WIP): Show guides, tips, tricks & more info events</span>",
+      "<span tooltip='Click on the Information top right of event (only DP, SM, CbC, OD, PoV & PoG)'>Event Info (WIP): Show guides, tips, tricks & more info on events</span>",
     default: true,
   };
   shouldRun() {
@@ -15,7 +17,11 @@ export default class EventInfo extends HHModule {
       location.pathname === "/event.html" ||
       location.pathname === "/home.html" ||
       location.pathname === "/path-of-glory.html" ||
-      location.pathname === "/path-of-valor.html"
+      location.pathname === "/path-of-valor.html" ||
+      location.pathname === "/seasonal.html" ||
+      location.pathname === "/world-boss-event" ||
+      location.pathname === "/season.html" ||
+      location.pathname === "/love-raids.html"
     );
   }
   run() {
@@ -26,6 +32,8 @@ export default class EventInfo extends HHModule {
 
     const path = location.pathname;
 
+    console.log("EventInfo module running on path:", path);
+
     if (path === "/home.html") {
       this.runHome();
       return;
@@ -34,8 +42,16 @@ export default class EventInfo extends HHModule {
       this.runEvent();
       return;
     }
-    if( path === "/path-of-glory.html" || path === "/path-of-valor.html"){
+    if (path === "/path-of-glory.html" || path === "/path-of-valor.html") {
       this.runPathes();
+      return;
+    }
+    if (path === "/seasonal.html") { // Montly Event
+      this.runSeasonal();
+      return;
+    }
+    if (path === "/season.html") {
+      this.runSeason();
       return;
     }
   }
@@ -47,5 +63,32 @@ export default class EventInfo extends HHModule {
   }
   runPathes() {
     new EventInfo_Pathes().run();
+  }
+  runSeasonal() {
+    new EventInfo_Seasonal().run();
+  }
+  runSeason() {
+    this.helperReplaceNotifButton(
+      "https://forum.kinkoid.com/index.php?/topic/31207-vademecum-rerum-gestarum-ex-haremverse-a-guide-to-the-events/#comment-310674"
+    );
+  }
+  runLoveRaids() {
+    this.helperReplaceNotifButton(
+      "https://forum.kinkoid.com/index.php?/topic/31207-vademecum-rerum-gestarum-ex-haremverse-a-guide-to-the-events/#comment-316577"
+    );
+  }
+
+  helperReplaceNotifButton(url: string) {
+    HHPlusPlusReplacer.doWhenSelectorAvailable(
+      ".button-notification-action.notif_button_s",
+      () => {
+        $(".button-notification-action.notif_button_s")
+          .attr("tooltip", "Several QoL: More Info on this event")
+          .off("click")
+          .on("click", (e) => {
+            GM.openInTab(url, { active: true });
+          });
+      }
+    );
   }
 }
---
+++ src/modules/EventInfo/EventInfo_Seasonal.ts
@@ -0,0 +1,41 @@
+import { SubModule } from "../../types/subModules";
+import { HHPlusPlusReplacer } from "../../utils/HHPlusPlusreplacer";
+
+export default class EventInfo_Seasonal implements SubModule {
+  private readonly EventInfoSeasonalLinks: string[] = [
+    "",
+    "https://forum.kinkoid.com/index.php?/topic/31207-vademecum-rerum-gestarum-ex-haremverse-a-guide-to-the-events/#comment-310068",
+    "",
+  ];
+  run() {
+    const currentSeasonalType = unsafeWindow.event_functionalities
+      ?.id_seasonal_event_type as number;
+    if (!currentSeasonalType || currentSeasonalType !== 2) {
+      //2 only for now
+      console.log("SE Info: No known seasonal event detected");
+      return;
+    }
+    console.log("SE Info: Known seasonal event detected:", currentSeasonalType);
+    this.replaceSeasonalNotifButton(this.EventInfoSeasonalLinks[currentSeasonalType - 1]);
+  }
+  replaceSeasonalNotifButton(link: string) {
+    if (!link) {
+      return;
+    }
+    HHPlusPlusReplacer.doWhenSelectorAvailable(
+      ".button-notification-action.notif_button_s",
+      () => {
+        $(".button-notification-action.notif_button_s")
+          .attr("tooltip", "Several QoL: More Info on this event")
+          .off("click")
+          .on("click", (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            GM.openInTab(link, {
+              active: true,
+            });
+          });
+      }
+    );
+  }
+}
