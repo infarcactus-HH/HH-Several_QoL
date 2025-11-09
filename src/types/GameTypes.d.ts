33298754b1908a35d9937872231c3496355cedcf
xnh0x <xnh0x@pm.me>
2025-11-09 14:04:55 +0100

    typo

--- src/types/GameTypes.d.ts
+++ src/types/GameTypes.d.ts
@@ -70,12 +70,12 @@ export interface PlacesOfPowerData {
 }
 export interface popupForQueue {
   popup: {
-    $dom_element: JQuery<HTMLElement>; // main injected dom element (?) still has to be attached thoguh it seems
+    $dom_element: JQuery<HTMLElement>; // main injected dom element (?) still has to be attached though it seems
     init: (t: boolean) => void; // only seen it called with false
     onOpen: () => void; // called after init
     addEventListeners: () => void; // called right after open
     popup_name: string;
-    close_on_esc: boolean; // wether it should close when pressing escape or not
+    close_on_esc: boolean; // whether it should close when pressing escape or not
     type: "toast" | "common" | "sliding" | "notification";
     onClose: () => void; // called when closing (for example when pressing escape if enabled)
     removeEventListeners: () => void; // called right after closing (even if close was called by destroy)
