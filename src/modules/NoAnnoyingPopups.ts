5b54febf548c7302ee77aa10d482c68465f4a89f
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-11-11 20:19:04 +0100

    Noannoyingpopup: Properly Sets/Unset the cookie and added a longer timeout for the cookie

--- src/modules/NoAnnoyingPopups.ts
+++ src/modules/NoAnnoyingPopups.ts
@@ -15,18 +15,55 @@ export default class NoAnnoyingPopups extends HHModule {
       return;
     }
     this.hasRun = true;
+    setCookie();
     //
-    try {
-      GM_cookie.set({
-        name: "disabledPopups",
-        value: "PassReminder,Bundles,News",
-        domain: location.hostname.split(".").slice(-2).join("."),
-        secure: true,
-        path: "/",
-        sameSite: "no_restriction",
-      });
-    } catch (e) {
-      document.cookie = "disabledPopups=PassReminder,Bundles,News; path=/";
+    function setCookie() {
+      console.log("Setting cookie to disable annoying popups");
+      try {
+        GM_cookie.set({
+          name: "disabledPopups",
+          value: "PassReminder,Bundles,News",
+          domain: location.hostname.split(".").slice(-2).join("."),
+          secure: true,
+          path: "/",
+          sameSite: "no_restriction",
+          expirationDate: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, // 30 days
+        });
+      } catch (e) {
+        document.cookie =
+          "disabledPopups=PassReminder,Bundles,News; path=/; max-age=" +
+          60 * 60 * 24 * 30;
+      }
+    }
+    if (location.pathname === "/home.html") {
+      console.log("Setting up listener to delete cookie on checkbox click");
+      $(document).on(
+        "change",
+        "input[name='severalQoL_noAnnoyingPopups']",
+        function (event) {
+          const isChecked = (event.target as HTMLInputElement).checked;
+          if (!isChecked) {
+            try {
+              GM_cookie.delete(
+                {
+                  name: "disabledPopups",
+                },
+                function (error: boolean) {
+                  if (error) {
+                    console.error("Error deleting cookie:", error);
+                  } else {
+                    console.log("Cookie deleted successfully");
+                  }
+                }
+              );
+            } catch (e) {
+              document.cookie = "disabledPopups=; path=/; max-age=0";
+            }
+          } else {
+            setCookie();
+          }
+        }
+      );
     }
   }
 }
