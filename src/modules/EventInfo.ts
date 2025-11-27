80107e104dfda495d0937c4f9c8ce607af001648
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2025-11-27 08:33:57 +0100

    Removed WIP from EventInfo

--- src/modules/EventInfo.ts
+++ src/modules/EventInfo.ts
@@ -9,7 +9,7 @@ export default class EventInfo extends HHModule {
   readonly configSchema = {
     baseKey: "eventInfo",
     label:
-      "<span tooltip='Click on the Information top right of event (only Mythic, DP, SM, CbC, OD, PoV & PoG)'>Event Info (WIP): Show guides, tips, tricks & more info on events</span>",
+      "<span tooltip='Click on the Information top right of event*'>Event Info : Show guides, tips, tricks & more info on events</span>",
     default: true,
   };
   static shouldRun() {
