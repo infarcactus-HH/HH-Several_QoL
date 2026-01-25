1ee86a93e73097c9057910aa4a10bd09a5f93ea3
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2026-01-25 23:29:00 +0100

    Moved StyleTweak css to a folder to match the rest of the script

---
+++ src/css/modules/StyleTweak/index.ts
@@ -0,0 +1,3 @@
+export { default as VillainReplaceBulbsByMulticolorBulbCss } from "./VillainReplaceBulbsByMulticolorBulb.css";
+export { default as povPoGHideClaimAllUntilLastDayCss } from "./PoVPoGHideClaimAllUntilLastDay.css";
+export { default as LustArenaStyleTweakCss } from "./LustArenaStyleTweak.css";
--- src/css/modules/index.ts
+++ src/css/modules/index.ts
@@ -1,4 +1,3 @@
-export { default as LustArenaStyleTweakCss } from "./LustArenaStyleTweak.css";
 export { default as villainShardTrackerCss } from "./VillainShardTracker.css";
 export { default as placesOfPowerCss } from "./PlacesOfPower++.css";
 export { default as meRankingInfoCss } from "./MERankingInfo.css";
@@ -7,8 +6,8 @@ export { default as loveRaidsCss } from "./LoveRaids.css";
 export { default as leagueOpponentHistoryCss } from "./LeagueOpponentHistory.css";
 export { default as eventInfoEventCss } from "./EventInfo/EventInfo_Event.css";
 export { default as eventInfoHomeCss } from "./EventInfo/EventInfo_Home.css";
-export { default as povPoGHideClaimAllUntilLastDayCss } from "./PoVPoGHideClaimAllUntilLastDay.css";
-export { default as VillainReplaceBulbsByMulticolorBulbCss } from "./VillainReplaceBulbsByMulticolorBulb.css";
 export { default as poVInfoCss } from "./PoVInfo.css";
 export { default as HHPlusPlusBdsmPatchPentaDrillCss } from "./HHPlusPlusBdsmPatch/PentaDrill.css";
 export { default as LabyTeamPresetPentaDrillCss } from "./LabyTeamPresets/PentaDrill.css";
+
+export * from "./StyleTweak";
