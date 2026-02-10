d816f355bb3e3ed0e72334c5392093f2bfd9973f
infarcactus-HH <223152632+infarcactus-HH@users.noreply.github.com>
2026-02-10 01:12:17 +0100

    [CompactLeagueStyleTweak] When reordering now snaps back to the top

--- src/modules/StyleTweak/CompactLeagueStyleTweak.ts
+++ src/modules/StyleTweak/CompactLeagueStyleTweak.ts
@@ -1,8 +1,17 @@
 import { SubModule } from "../../base";
 import { CompactLeagueStyleTweakCss } from "../../css/modules";
+import { HHPlusPlusReplacer } from "../../utils/HHPlusPlusreplacer";
 
 export default class CompactLeagueStyleTweak implements SubModule {
-  async run() {
+  run() {
+    this.injectCSS();
+    HHPlusPlusReplacer.doWhenSelectorAvailable(".league_table > .data-list", ($el) => {
+      new MutationObserver(() => {
+        $el.parent()[0].scrollTo({ top: 0, behavior: "instant" });
+      }).observe($el[0], { childList: true });
+    });
+  }
+  async injectCSS() {
     GM_addStyle(CompactLeagueStyleTweakCss);
   }
 }
